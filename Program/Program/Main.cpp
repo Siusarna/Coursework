66ce8d0c74e3ac14e9c55755d366189f6a40e760
Siusarna <charodeyrap@gmail.com>
2019-03-24 23:12:17 +0200

    Update Main.cpp

--- Program/Program/Main.cpp
+++ Program/Program/Main.cpp
@@ -8,17 +8,5 @@ using namespace std;
 int main() {
 	vector<string> arr;
 	arr = Open_Parce("dict_processed.txt");
-	Queue que;
-	que.add("5");
-	que.add("10");
-	que.add("11");
-	string output;
-	if (que.pop(output)) cout << output << endl;
-	if (que.pop(output)) cout << output << endl;
-	if (que.pop(output)) cout << output << endl;
-	if (que.pop(output)) cout << output << endl;
-	que.add("11");
-	if (que.pop(output)) cout << output << endl;
-	cout << que.isEmpty() << endl;
 	system("pause");
 }
